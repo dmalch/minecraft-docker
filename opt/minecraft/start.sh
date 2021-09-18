5fe7a2a2941ae91ab3ecfbbbf8032da8d61b3a9f
Dmitry Malchikov <dmalch@gmail.com>
2021-09-18 14:11:32 +0200

    Sync only logs and world data

--- opt/minecraft/start.sh
+++ opt/minecraft/start.sh
@@ -2,11 +2,11 @@
 set -e
 
 if [ ! -z "$S3_BUCKET" ]; then
-    aws s3 sync s3://$S3_BUCKET /opt/minecraft
+    aws s3 sync --exclude "*" --include "world/*" --include "logs/*" s3://$S3_BUCKET /opt/minecraft
 
     # Cron job to sync data to S3 every five mins
     cat <<CRON >/etc/cron.d/minecraft
-*/5 * * * * root aws s3 sync /opt/minecraft s3://$S3_BUCKET
+*/5 * * * * root aws s3 sync --exclude "*" --include "world/*" --include "logs/*" /opt/minecraft s3://$S3_BUCKET
 CRON
     service cron start
 fi
